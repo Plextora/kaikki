323bc1051f0a0261f48893042071e4899bf55b50
Plextora <71889427+Plextora@users.noreply.github.com>
2022-12-26 14:19:09 -0500

    Added DiscordRPC

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
+using DiscordRPC;
+
+DiscordRpcClient client;
+client = new DiscordRpcClient("932775385014353970");
+client.Initialize();
 
 [DllImport("user32.dll")]
 static extern IntPtr GetForegroundWindow();
@@ -26,4 +31,31 @@ string GetForegroundProcessName()
     return "Unknown";
 }
 
+void RunDiscordRpc()
+{
+    client.SetPresence(new RichPresence
+    {
+        Details = $"This user is using {GetForegroundProcessName()}",
+        State = "Made by Plextora#0033",
+        Assets = new Assets
+        {
+            LargeImageKey = "kaikki_logo",
+            LargeImageText = "Kaikki Logo"
+        }
+    });
+}
+
+void StopDiscordRpc()
+{
+    client.ClearPresence();
+    client.Deinitialize();
+    client.Dispose();
+}
+
 Console.WriteLine($"Focused process name: {GetForegroundProcessName()}");
+
+RunDiscordRpc();
+
+Console.ReadLine();
+
+StopDiscordRpc();
