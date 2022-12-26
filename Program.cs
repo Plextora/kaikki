075a76f6d9031c66bb72befd064d2702daf82a86
Plextora <71889427+Plextora@users.noreply.github.com>
2022-12-26 14:11:12 -0500

    Program can now get the name of the current focused process

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+[DllImport("user32.dll")]
+static extern IntPtr GetForegroundWindow();
+
+[DllImport("user32.dll")]
+static extern Int32 GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+string GetForegroundProcessName()
+{
+    IntPtr hwnd = GetForegroundWindow();
+    
+    if (hwnd == IntPtr.Zero)
+        return "Unknown";
+
+    uint pid;
+    GetWindowThreadProcessId(hwnd, out pid);
+
+    foreach (Process p in Process.GetProcesses())
+    {
+        if (p.Id == pid)
+            return p.ProcessName;
+    }
+
+    return "Unknown";
+}
+
+Console.WriteLine($"Focused process name: {GetForegroundProcessName()}");
